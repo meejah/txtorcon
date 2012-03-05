1eea678edc5b0fc8d15b9f5f396e68212a7c07d6
meejah <meejah@meejah.ca>
2012-03-05 09:19:53 -0700

    add fix for .onion, .exit parsing errors

--- txtorcon/stream.py
+++ txtorcon/stream.py
@@ -13,6 +13,17 @@ from twisted.python import log
 from txtorcon.interface import ICircuitContainer, IStreamListener
 import ipaddr
 
+def maybe_ip_addr(addr):
+    """
+    Tries to return an IPAddress, otherwise returns a string. I could
+    explicitly check for .exit or .onion at the end instead.
+    """
+    
+    try:
+        return ipaddr.IPAddress(addr)
+    except ValueError:
+        return str(addr)
+
 class Stream(object):
     """
     Represents an active stream in Tor's state (:class:`txtorcon.TorState`).
@@ -129,7 +140,7 @@ class Stream(object):
             last_colon = kw['SOURCE_ADDR'].rfind(':')
             self.source_addr = kw['SOURCE_ADDR'][:last_colon]
             if self.source_addr != '(Tor_internal)':
-                self.source_addr = ipaddr.IPAddress(self.source_addr)
+                self.source_addr = maybe_ip_addr(self.source_addr)
             self.source_port = int(kw['SOURCE_ADDR'][last_colon+1:])
 
         self.state = args[1]
@@ -148,7 +159,7 @@ class Stream(object):
                 [x.stream_succeeded(self) for x in self.listeners]
             
         elif self.state == 'REMAP':
-            self.target_addr = ipaddr.IPAddress(args[3][:args[3].rfind(':')])
+            self.target_addr = maybe_ip_addr(args[3][:args[3].rfind(':')])
 
         elif self.state == 'CLOSED':
             if self.circuit:
