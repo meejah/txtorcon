cd113ca13225fe32c86421a7bb8d1a6cfe6b4d56
meejah <meejah@meejah.ca>
2012-05-24 10:50:13 -0600

    fix setup.py to include static single-page documentation, as well as Sphinx sources for the docs

--- setup.py
+++ setup.py
@@ -30,10 +30,22 @@ setup(name = 'txtorcon',
 #      scripts = ['examples/attach_streams_by_country.py'],
 
       ## I'm a little unclear if I'm doing this "properly", especially
-      ## the documentation etc.
-      
-      data_files = [('share/txtorcon', ['README', 'TODO']),
-##FIXME                    ('share/txtorcon', ['doc_html/index.html', 'doc_html/objects.inv'] + map(lambda x: os.path.join('doc_html/_static', x), os.listdir('doc_html/_static'))),
+      ## the documentation etc. Do we really want "share/txtorcon" for
+      ## the first member of the tuple?
+
+      data_files = [('share/txtorcon', ['INSTALL', 'README', 'TODO', 'meejah.asc']),
+                    
+                    ## this includes pre-built single-page HTML docs
+                    ## into the distribution. the map construct grabs
+                    ## everything in doc_html/_static
+                    ('share/txtorcon', ['doc_html/index.html', 'doc_html/objects.inv'] + map(lambda x: os.path.join('doc_html/_static', x), os.listdir('doc_html/_static'))),
+
+                    ## this includes the Sphinx source for the
+                    ## docs. The "map+filter" construct grabs all .rst
+                    ## files and re-maps the path
+                    ('share/txtorcon', ['docs/apilinks_sphinxext.py', 'docs/conf.py', 'docs/Makefile', 'docs/avatar.png'] + map(lambda x: os.path.join('docs', x), filter(lambda x: x[-3:] == 'rst', os.listdir('docs'))) + map(lambda x: os.path.join('docs/_static', x), os.listdir('docs/_static'))),
+
+                    ## include all the examples
                     ('share/txtorcon/examples', map(lambda x: os.path.join('examples', x), filter(lambda x: x[-3:] == '.py', os.listdir('examples'))))
                     ]
       )
