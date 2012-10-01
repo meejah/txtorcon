792d27425b7a5d8f55be7f9bef239455ed5c217f
meejah <meejah@meejah.ca>
2012-10-01 00:52:29 -0600

    Progress towards a real app, incorporate some changes from the passive monitor and deal with a number of outstanding circuits at once (although this needs to properly count the number of circuits we've tried to build per-node, as it's not counting all the outstanding ones yet)

--- apps/exit_scanner/failure-rate-scanner.py
+++ apps/exit_scanner/failure-rate-scanner.py
@@ -35,7 +35,7 @@ import math
 
 from twisted.internet import reactor, task
 from twisted.internet.endpoints import TCP4ClientEndpoint
-from twisted.python import usage
+from twisted.python import usage, failure
 from zope.interface import implements
 
 import txtorcon
@@ -44,9 +44,9 @@ class Options(usage.Options):
 
     optParameters = [
         ['guards', 'G', 10, 'Total number of Guards to choose.', int],
-        ['circuits', 'c', 100, 'Total number of circuits to set up (distributed evenly over Guards).', int],
-        ['connect', 'c', "localhost:9051", 'Tor control socket to connect to in host:port format, like "localhost:9051" (the default).'],
-        ['rate', 'r', 10, 'Number of circuits to set up per minute.', int],
+        ['circuits', 'C', 100, 'Total number of circuits to set up (distributed evenly over Guards).', int],
+        ['address', 'a', "localhost:9051", 'Tor control socket to connect to in host:port format, like "localhost:9051" (the default).'],
+        ['max-outstanding', 'x', 10, "Maximum number of circuits we'll ask Tor to build at one time."]
         ]
 
     def __init__(self):
@@ -56,10 +56,12 @@ class GuardStatistics:
     id_hex = ''
     failure = 0
     success = 0
+    errors = 0
+    nopath = 0
 
 class CircuitCreator(txtorcon.CircuitListenerMixin):
 
-    def __init__(self, reactor, tor_protocol, guards, total_circuits):
+    def __init__(self, reactor, tor_protocol, guards, total_circuits, max_outstanding):
         """
         :param tor_protocol:
             (a TorControlProtocol instance) must be bootstrapped
@@ -70,19 +72,26 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
 
         :param total_circuits:
             total number of circuits to ask Tor to create
+
+        :param max_outstanding:
+            the number of circuits we'll ask Tor to build at once
+            (i.e. until we get a failed or built status for a circuit,
+            it's outstanding)
         """
 
         """reactor to use; useful if we want to write tests"""
         self.reactor = reactor
 
-        """TorState instance, should be bootstrapped already"""
+        """TorProtocol instance, should be bootstrapped already"""
         self.protocol = tor_protocol
 
-        """A list of all the Router instances we will use as Guards (must actually have the Guard flag or Tor will probabyl reject them)"""
+        """A list of all the Router instances we will use as Guards (must actually have the Guard flag or Tor will probabyl reject them). This list contains just the hex IDs as strings."""
         self.guards = guards
 
         """Total number of circuits we'll create"""
         self.total_circuits = total_circuits
+
+        self.max_outstanding = max_outstanding
 
         """per-guard statistics"""
         self.statistics = {}
@@ -90,11 +99,27 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         self.circuits_per_guard = math.ceil(float(self.total_circuits) / float(len(guards)))
         if self.circuits_per_guard < 1:
             raise RuntimeError("Not enough circuits (%d) for even one per guard (%d)." % (self.total_circuits, len(guards)))
-        
+
         self.current_guard = None
         self.current_guard_circuits = 0
         self.last_circuit_extend = 0
+        self.outstanding_circuits = []
         self._try_next_guard()
+
+    def dump_statistics(self):
+        print "statistics:"
+        with open('stats.data', 'w') as statsfile:
+            statsfile.write('#guard_id built failed success_rate create_errors NOPATH\n\n')
+
+            for (hexid, stats) in self.statistics.items():
+                print " ",hexid
+                rate = 0.0
+                if stats.success + stats.failure > 0:
+                    rate = float(stats.failure) / float(stats.success + stats.failure)
+                print "    built:",stats.success,"failed:",stats.failure,"failure rate:",(rate*100.0),"%",
+                print "  errors=%d, NOPATH=%d" % (stats.errors, stats.nopath)
+
+                statsfile.write('%s %d %d %f %d %d\n' % (hexid, stats.success, stats.failure, rate, stats.errors, stats.nopath))
 
     def _error(self, f):
         print "ERROR",f
@@ -107,12 +132,9 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         """
 
         if len(self.guards) == 0:
-            print "no more guards, we're done?"
-            print "statistics:"
-            for (hexid, stats) in self.statistics.items():
-                print " ",hexid
-                rate = float(stats.failure) / float(stats.success + stats.failure)
-                print "    built:",stats.success,"failed:",stats.failure,"failure rate:",(rate*100.0),"%"
+            print "no more guards, we're done"
+            ## note we registered a shutdown method to dump the
+            ## statistics (which also works for ctrl-c for example)
             reactor.stop()
             return
 
@@ -122,35 +144,53 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         gs = GuardStatistics()
         gs.id_hex = self.current_guard
         self.statistics[self.current_guard] = gs
-        
+
         print "setting up to use guard:",self.current_guard
         d = self.protocol.set_conf("StrictNodes", 1, "EntryNodes", str(self.current_guard))
         d.addCallback(self._setup_circuit).addErrback(self._error)
         return d
 
     def _setup_circuit(self, arg):
-        print "_setup_circuit():",arg
+        ##print "_setup_circuit():",arg
+
+        # note 100% sure why I'm getting "551 Couldn't start circuit"
+        # failures on creating a new circuit, but this waits at least
+        # 2 seconds before doing another setup on two errors in a row
+        if isinstance(arg, failure.Failure):
+            self.statistics[self.current_guard].errors += 1
+
+            timediff = self.reactor.seconds() - self.last_circuit_extend
+            if timediff < 2.0:
+                print arg.getErrorMessage(),
+                print "-- waiting",(2.0 - timediff),"seconds"
+                self.reactor.callLater(2.0 - timediff, self._setup_circuit, arg)
+                return None
+
+        elif arg and arg.split()[0] == 'EXTENDED':
+            circid = int(arg.split()[1])
+            if circid in self.outstanding_circuits:
+                print "WEIRD, already have",circid,"in outstanding list."
+            else:
+                self.outstanding_circuits.append(circid)
+
+            if len(self.outstanding_circuits) >= self.max_outstanding:
+                return
+
         # tell Tor to build a new circuit, and we don't care about the
         # path. Since we've set only one guard, though, and
-        # strictguardnodes we should get a random (albeit
-        # tor-selected) route using that guard.
+        # StrictNodes we should get a random (albeit tor-selected)
+        # route using that guard.
 
-        timediff = self.reactor.seconds() - self.last_circuit_extend
-        if timediff < 2.0:
-            print "waiting",(2.0 - timediff),"seconds"
-            self.reactor.callLater(2.0 - timediff, self._setup_circuit, arg)
-            return None
-        
         self.last_circuit_extend = self.reactor.seconds()
-        d = self.protocol.queue_command("EXTENDCIRCUIT 0 purpose=controller")
-        d.addErrback(self._setup_circuit)
+        d = self.protocol.queue_command("EXTENDCIRCUIT 0")
+        d.addBoth(self._setup_circuit)
         return None
 
     def _increment_circuit_count(self):
-        print "INCrement"
-        # we successfully set up a circuit. if we've set up enough for
-        # this guard already, go to the next guard. if not, construct
-        # another for this guard.
+        # we got notification about a circuit (built fine, or
+        # failed). if we've tried enough for this guard already, go to
+        # the next guard. if we're out of guards, that method will
+        # dump stats and stop the reactor
 
         self.current_guard_circuits += 1
         print "We've tried",self.current_guard_circuits,"for guard",self.current_guard
@@ -163,31 +203,71 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
 
     def circuit_built(self, circuit):
         print "BUILT",circuit
-        if len(circuit.path) and circuit.path[0].id_hex[1:] == self.current_guard:
-            print "DINGDINGDING!"
+        build_again = False
+
+        if circuit.id in self.outstanding_circuits:
+            if len(self.outstanding_circuits) >= self.max_outstanding:
+                build_again = True
+            ##print "   found it",circuit.id
+            self.outstanding_circuits.remove(circuit.id)
+
+        if len(circuit.path) and circuit.path[0].id_hex == self.current_guard:
+            ##print "DINGDINGDING!"
             self.statistics[self.current_guard].success += 1
             self._increment_circuit_count()
+
+        print self.current_guard, "awaiting:", self.outstanding_circuits
+        if build_again:
+            return self._setup_circuit(None)
 
     def circuit_closed(self, circuit):
         pass
 
     def circuit_failed(self, circuit, reason):
-        print "FAILED:",circuit,reason
+        if reason == 'MEASUREMENT_FAILED':
+            print "measurement failed, skipping failure",circuit
+            return
+
+        build_again = False
+
+        if circuit.id in self.outstanding_circuits:
+            if len(self.outstanding_circuits) >= self.max_outstanding:
+                build_again = True
+            self.outstanding_circuits.remove(circuit.id)
+
         if len(circuit.path) and circuit.path[0].id_hex[1:] == self.current_guard:
-            print "Oh Noes!"
             self.statistics[self.current_guard].failure += 1
             self._increment_circuit_count()
 
+        if reason == 'NOPATH':
+            # presuming it's the current guard's circuit that failed...
+            self.statistics[self.current_guard].nopath += 1
+
+        print self.current_guard, "awaiting:", self.outstanding_circuits
+        if build_again:
+            return self._setup_circuit(None)
 
 def setup(processprotocol):
     proto = processprotocol.tor_protocol
     state = txtorcon.TorState(proto)
     state.post_bootstrap.addCallback(really_setup).addErrback(setup_failed)
 
+creator = None
 def really_setup(state):
     print 'Connected to a Tor version %s' % state.protocol.version
-    creator = CircuitCreator(reactor, state.protocol, ['11A0239FC6668705F68842811318B669C636F86E', 'D5EDC74F2FB81E6AC1A8EBA56448F71DDFAA4AE5'], 10)
+    guards = state.guards.keys()
+    global creator
+    creator = CircuitCreator(reactor, state.protocol, guards[:10], 100, 5)
+    print "circuits per guard:", creator.circuits_per_guard
     state.add_circuit_listener(creator)
+
+def on_shutdown(*args):
+    global creator
+    if creator:
+        print "shutting down, dumping statistics:"
+        creator.dump_statistics()
+reactor.addSystemEventTrigger('before', 'shutdown', on_shutdown)
+    
 
 def setup_failed(arg):
     print "SETUP FAILED",arg
