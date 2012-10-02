fec0affa693755719eb9e483c0a4f14232aa5da3
mike warren <meejah@gmail.com>
2012-10-01 23:15:49 -0600

    Much nicer output, put router names + hexid in stats, dump statistics after every router.

--- apps/exit_scanner/failure-rate-scanner.py
+++ apps/exit_scanner/failure-rate-scanner.py
@@ -41,6 +41,8 @@ from zope.interface import implements
 
 import txtorcon
 
+PRINT_ERRORS = False
+
 class Options(usage.Options):
 
     optFlags = [
@@ -72,6 +74,14 @@ class GuardStatistics:
     success = 0
     errors = 0
     nopath = 0
+    guard = None                        # will be Router instance
+
+    def failure_rate(self):
+        rate = 0.0
+        if self.success + self.failure > 0:
+            rate = float(self.failure) / float(self.success + self.failure)
+        return rate
+
 
 class CircuitCreator(txtorcon.CircuitListenerMixin):
 
@@ -108,7 +118,7 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         self.max_outstanding = max_outstanding
 
         """per-guard statistics"""
-        self.statistics = {}
+        self.statistics = {}            # key is router hex-id
 
         self.circuits_per_guard = math.ceil(float(self.total_circuits) / float(len(guards)))
         if self.circuits_per_guard < 1:
@@ -118,22 +128,25 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         self.current_guard_circuits = 0
         self.last_circuit_extend = 0
         self.outstanding_circuits = []
+        self.outstanding_deferreds = []
+        self.completed_circuits = []
         self._try_next_guard()
 
+    @staticmethod
+    def dump_statistic(stats):
+        print " ", stats.guard.id_hex, stats.guard.unique_name
+        rate = stats.failure_rate()
+        print "    built:",stats.success,"failed:",stats.failure,"failure rate:",(rate*100.0),"%",
+        print "  errors=%d, NOPATH=%d" % (stats.errors, stats.nopath)
+        return rate
+
     def dump_statistics(self):
-        print "statistics:"
         with open('stats.data', 'w') as statsfile:
-            statsfile.write('#guard_id built failed success_rate guard_bandwidth create_errors NOPATH\n\n')
+            statsfile.write('#guard_unique_name guard_id built failed success_rate guard_bandwidth create_errors NOPATH\n\n')
 
             for (hexid, stats) in self.statistics.items():
-                print " ",hexid
-                rate = 0.0
-                if stats.success + stats.failure > 0:
-                    rate = float(stats.failure) / float(stats.success + stats.failure)
-                print "    built:",stats.success,"failed:",stats.failure,"failure rate:",(rate*100.0),"%",
-                print "  errors=%d, NOPATH=%d" % (stats.errors, stats.nopath)
-
-                statsfile.write('%s %d %d %f %f %d %d\n' % (hexid, stats.success, stats.failure, rate, stats.guard.bandwidth, stats.errors, stats.nopath))
+                rate = stats.failure_rate()
+                statsfile.write('%s %s %d %d %f %f %d %d\n' % (stats.guard.unique_name, hexid, stats.success, stats.failure, rate, stats.guard.bandwidth, stats.errors, stats.nopath))
 
     def _error(self, f):
         print "ERROR",f
@@ -149,6 +162,8 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
             print "no more guards, we're done"
             ## note we registered a shutdown method to dump the
             ## statistics (which also works for ctrl-c for example)
+            if len(self.outstanding_deferreds):
+                return
             reactor.stop()
             return
 
@@ -159,7 +174,8 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         gs.guard = self.current_guard
         self.statistics[self.current_guard.id_hex] = gs
 
-        print "setting up to use guard:",self.current_guard.name
+        sys.stdout.write('%42s ' % self.current_guard.unique_name)
+        sys.stdout.flush()
         d = self.protocol.set_conf("StrictNodes", 1, "EntryNodes", str(self.current_guard.id_hex))
         d.addCallback(self._setup_circuit).addErrback(self._error)
         return d
@@ -175,8 +191,9 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
 
             timediff = self.reactor.seconds() - self.last_circuit_extend
             if timediff < 2.0:
-                print arg.getErrorMessage(),
-                print "-- waiting",(2.0 - timediff),"seconds"
+                if PRINT_ERRORS:
+                    print arg.getErrorMessage(),
+                    print "-- waiting",(2.0 - timediff),"seconds"
                 self.reactor.callLater(2.0 - timediff, self._setup_circuit, arg)
                 return None
 
@@ -196,13 +213,20 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         # route using that guard.
 
         if self.current_guard_circuits + len(self.outstanding_circuits) >= self.circuits_per_guard:
-            print "already requested enough"
+            ##print "already requested enough"
             return
 
         self.last_circuit_extend = self.reactor.seconds()
         d = self.protocol.queue_command("EXTENDCIRCUIT 0")
+        self.outstanding_deferreds.append(d)
+        d.addBoth(self._remove_completed_deferred, d)
         d.addBoth(self._setup_circuit)
         return None
+
+    def _remove_completed_deferred(self, arg, d):
+        ##print "remove",arg
+        self.outstanding_deferreds.remove(d)
+        return arg
 
     def _increment_circuit_count(self):
         # we got notification about a circuit (built fine, or
@@ -211,16 +235,19 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
         # dump stats and stop the reactor
 
         self.current_guard_circuits += 1
-        print "We've tried",self.current_guard_circuits,"for guard",self.current_guard.unique_name
+        ##print "We've tried",self.current_guard_circuits,"for guard",self.current_guard.unique_name
         if self.current_guard_circuits >= self.circuits_per_guard:
-            print "rotating guard"
+            ##print "rotating guard"
+            stat = self.statistics[self.current_guard.id_hex]
+            print ' %d/%d failed; %3.1f%% failure-rate' % (stat.failure, stat.success+stat.failure, stat.failure_rate()*100.0)
+            self.dump_statistics()
             return self._try_next_guard()
 
     ## ICircuitListener API (only implementing the ones we want,
     ## thanks to CircuitListenerMixin)
 
     def circuit_built(self, circuit):
-        print "BUILT",circuit
+        #print "BUILT",circuit
         build_again = False
 
         if circuit.id in self.outstanding_circuits:
@@ -228,13 +255,22 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
                 build_again = True
             ##print "   found it",circuit.id
             self.outstanding_circuits.remove(circuit.id)
+            self.completed_circuits.append(circuit.id)
+
+        else:
+            if circuit.id in self.completed_circuits:
+                msg = "ERROR? got a BUILT after being done with a circuit: %s\n" % str(circuit)
+                print msg,
+                sys.stderr.write(msg)
 
         if len(circuit.path) and circuit.path[0].id_hex == self.current_guard.id_hex:
             ##print "DINGDINGDING!"
             self.statistics[self.current_guard.id_hex].success += 1
+            sys.stdout.write('.')
+            sys.stdout.flush()
             self._increment_circuit_count()
 
-        print self.current_guard.unique_name, "awaiting:", self.outstanding_circuits
+        ##print self.current_guard.unique_name, "awaiting:", self.outstanding_circuits
         if build_again:
             return self._setup_circuit(None)
 
@@ -247,21 +283,28 @@ class CircuitCreator(txtorcon.CircuitListenerMixin):
             return
 
         build_again = False
+        ##print "FAIL", reason, circuit.id, '->'.join(map(lambda x: x.unique_name, circuit.path))
 
         if circuit.id in self.outstanding_circuits:
             if len(self.outstanding_circuits) >= self.max_outstanding:
                 build_again = True
             self.outstanding_circuits.remove(circuit.id)
-
-        if len(circuit.path) and circuit.path[0].id_hex[1:] == self.current_guard.id_hex:
             self.statistics[self.current_guard.id_hex].failure += 1
+            sys.stdout.write('F')
+            sys.stdout.flush()
             self._increment_circuit_count()
+
+        else:
+            if circuit.id in self.completed_circuits:
+                msg = "ERROR? got a FAILED after being done with a circuit: %s\n" % str(circuit)
+                print msg,
+                sys.stderr.write(msg)
 
         if reason == 'NOPATH':
             # presuming it's the current guard's circuit that failed...
             self.statistics[self.current_guard.id_hex].nopath += 1
 
-        print self.current_guard.unique_name, "awaiting:", self.outstanding_circuits
+        ##print self.current_guard.unique_name, "awaiting:", self.outstanding_circuits
         if build_again:
             return self._setup_circuit(None)
 
