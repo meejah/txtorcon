4ce1f77154085e83979d055b5a58f4fe64cc603a
meejah <meejah@meejah.ca>
2013-01-09 18:37:03 -0700

    pyflakes, pep8 cleanup and use build_local_tor_connection() instead

--- examples/circuit_for_next_stream.py
+++ examples/circuit_for_next_stream.py
@@ -8,27 +8,23 @@
 ## high-uptime nodes"
 ##
 
-import os
 import sys
-import stat
 import functools
 
 from twisted.python import log
-from twisted.internet import reactor, defer
-from twisted.internet.endpoints import UNIXClientEndpoint
-from twisted.internet.endpoints import TCP4ClientEndpoint
+from twisted.internet import reactor
 from zope.interface import implements
 
 import txtorcon
 
+
 class MyStreamListener(txtorcon.StreamListenerMixin):
-    
+
     def stream_new(self, stream):
-        print "new stream:",stream.id,stream.target_host
-    
+        print "new stream:", stream.id, stream.target_host
+
     def stream_succeeded(self, stream):
-        print "successful stream:",stream.id,stream.target_host
-    
+        print "successful stream:", stream.id, stream.target_host
 
 
 class MyAttacher(txtorcon.CircuitListenerMixin, txtorcon.StreamListenerMixin):
@@ -66,14 +62,15 @@ class MyAttacher(txtorcon.CircuitListenerMixin, txtorcon.StreamListenerMixin):
         return None
 
     def stream_attach(self, stream, circuit):
-        print "stream",stream.id,"attached to circuit",circuit.id, \
-              "with path:",'->'.join(map(lambda x: x.location.countrycode, circuit.path))
+        print "stream", stream.id, "attached to circuit", circuit.id, \
+              "with path:", '->'.join(map(lambda x: x.location.countrycode, circuit.path))
         if self.circuit is circuit:
             print "...so we're done."
             reactor.stop()
 
+
 def do_setup(path, state):
-    print "Connected to a Tor version",state.protocol.version
+    print "Connected to a Tor version", state.protocol.version
 
     attacher = MyAttacher(state)
     state.set_attacher(attacher, reactor)
@@ -83,30 +80,33 @@ def do_setup(path, state):
     print "Existing state when we connected:"
     print "Streams:"
     for s in state.streams.values():
-        print ' ',s
+        print ' ', s
 
     print
     print "General-purpose circuits:"
     for c in filter(lambda x: x.purpose == 'GENERAL', state.circuits.values()):
-        print ' ',c.id,'->'.join(map(lambda x: x.location.countrycode, c.path))
+        print ' ', c.id, '->'.join(map(lambda x: x.location.countrycode, c.path))
 
     print "Building our Circuit:", path
-    path = map(lambda x: state.routers[x], path)
+    try:
+        path = map(lambda x: state.routers[x], path)
+    except KeyError, e:
+        print "Couldn't find router:", e
+        sys.exit(1)
     print "...using routers:", path
     return state.build_circuit(path).addCallback(attacher.set_circuit).addErrback(log.err)
 
-def setup_failed(arg):
-    print "SETUP FAILED",arg
-    reactor.stop()
 
-point = TCP4ClientEndpoint(reactor, "localhost", 9051)
+def setup_failed(arg):
+    print "Setup Failed:", arg.getErrorMessage()
+    reactor.stop()
 
 if len(sys.argv) == 1:
     print "usage: %s router [router] [router] ..." % sys.argv[0]
     sys.exit(1)
 
 path = sys.argv[1:]
-    
-d = txtorcon.build_tor_connection(point)
+
+d = txtorcon.build_local_tor_connection(reactor)
 d.addCallback(functools.partial(do_setup, path)).addErrback(setup_failed)
 reactor.run()
