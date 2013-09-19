c8bfa5cc4fb5b419103987b45caef8922b6c59ca
Kevin Butler <haqkrs@gmail.com>
2013-09-19 06:58:50 +0100

    Temp fix for "missing package error" during pip install

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(name = 'txtorcon',
       author_email = __contact__,
       url = __url__,
       license = __license__,
-      packages  = ["txtorcon", "txtorcon.test"],
+      packages  = ["txtorcon"],
 #      scripts = ['examples/attach_streams_by_country.py'],
 
       ## I'm a little unclear if I'm doing this "properly", especially
