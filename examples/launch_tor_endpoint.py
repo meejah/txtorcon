b3fc68359ece5e1b8a4847d3f3bca25c49544264
David Stainton <dstainton415@gmail.com>
2014-05-05 22:24:52 +0000

    Update example to use endpoint parser

--- examples/launch_tor_endpoint.py
+++ examples/launch_tor_endpoint.py
@@ -3,14 +3,16 @@
 ##
 ## Here we set up a Twisted Web server and then launch a slave tor
 ## with a configured hidden service directed at the Web server we set
-## up. This uses TCPHiddenServiceEndpoint, which gives you slightly
-## less control over how things are set up, but may be easier. See
-## also the :ref:`launch_tor.py` example.
+## up. This uses serverFromString to translate the "onion" endpoint descriptor
+## into a TCPHiddenServiceEndpoint object...
 ##
 
+import shutil
 
 from twisted.internet import reactor
 from twisted.web import server, resource
+from twisted.internet.endpoints import serverFromString
+
 import txtorcon
 
 
@@ -22,40 +24,23 @@ class Simple(resource.Resource):
 
 site = server.Site(Simple())
 
-
 def setup_failed(arg):
     print "SETUP FAILED", arg
-    reactor.stop()
-
 
 def setup_complete(port):
-    print "I have set up a hidden service, advertised at:"
+    print "I have set up a hidden service, advertised at: %s" % (port,)
     print "http://%s:%d" % (port.onion_uri, port.onion_port)
     print "locally listening on", port.getHost()
 
+def progress(percent, tag, message):
+    bar = int(percent / 10)
+    print '[%s%s] %s' % ('#' * bar, '.' * (10 - bar), message)
 
-def setup_hidden_service(tor_process_protocol):
-    config = txtorcon.TorConfig(tor_process_protocol.tor_protocol)
-    public_port = 80
-    hs_endpoint = txtorcon.TCPHiddenServiceEndpoint(reactor, config,
-                                                    public_port)
+hs_endpoint = serverFromString(reactor, "onion:publicPort=80")
+#hs_endpoint = serverFromString(reactor, "onion:controlPort=9089:localPort=8080:publicPort=80")
+#hs_endpoint = serverFromString(reactor, "onion:controlPort=9089:localPort=8080:publicPort=80:hiddenServiceDir=/home/human/src/txtorcon/hidserv")
 
-    ## the important thing here is that "site" implements
-    ## IProtocolFactory -- this could be any service at all,
-    ## obviously.
-    hs_endpoint.listen(site).addCallback(setup_complete).addErrback(setup_failed)
+d = hs_endpoint.listen(site)
+d.addCallbacks(setup_complete, setup_failed)
 
-
-def updates(prog, tag, summary):
-    print "%d%%: %s" % (prog, summary)
-
-# set a couple options to avoid conflict with the defaults if a Tor is
-# already running
-config = txtorcon.TorConfig()
-config.SOCKSPort = 0
-config.ControlPort = 9089
-
-d = txtorcon.launch_tor(config, reactor, progress_updates=updates, timeout=60)
-d.addCallback(setup_hidden_service)
-d.addErrback(setup_failed)
 reactor.run()
