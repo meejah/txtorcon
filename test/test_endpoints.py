c02b63c145371b307465e71345f81b24beb28f55
meejah <meejah@meejah.ca>
2014-06-13 22:01:53 -0600

    pep8 cleanups

--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -10,9 +10,16 @@ from zope.interface import implements
 from twisted.trial import unittest
 from twisted.test import proto_helpers
 from twisted.internet import defer, error, task, tcp
-from twisted.internet.endpoints import TCP4ServerEndpoint, serverFromString, clientFromString
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.endpoints import serverFromString
+from twisted.internet.endpoints import clientFromString
 from twisted.python.failure import Failure
-from twisted.internet.interfaces import IReactorCore, IProtocolFactory, IProtocol, IReactorTCP, IListeningPort, IAddress
+from twisted.internet.interfaces import IReactorCore
+from twisted.internet.interfaces import IProtocolFactory
+from twisted.internet.interfaces import IProtocol
+from twisted.internet.interfaces import IReactorTCP
+from twisted.internet.interfaces import IListeningPort
+from twisted.internet.interfaces import IAddress
 
 from txtorcon import TorControlProtocol
 from txtorcon import ITorControlProtocol
@@ -52,7 +59,8 @@ class EndpointTests(unittest.TestCase):
         # now if we specify a control_port it should be an error since
         # the above should have launched one.
         try:
-            config1 = yield get_global_tor(Mock(), control_port=111, _tor_launcher=lambda x, y, z: True)
+            config1 = yield get_global_tor(Mock(), control_port=111,
+                                           _tor_launcher=lambda x, y, z: True)
             self.fail()
         except RuntimeError as e:
             # should be an error
@@ -72,7 +80,8 @@ class EndpointTests(unittest.TestCase):
         m = Mock()
         from txtorcon import endpoints
         endpoints.launch_tor = m
-        ep = yield TCPHiddenServiceEndpoint.private_tor(Mock(), 80, control_port=1234)
+        ep = yield TCPHiddenServiceEndpoint.private_tor(Mock(), 80,
+                                                        control_port=1234)
         m.assert_called()
 
     @defer.inlineCallbacks
@@ -86,6 +95,7 @@ class EndpointTests(unittest.TestCase):
     @defer.inlineCallbacks
     def test_system_tor(self):
         from test_torconfig import FakeControlProtocol
+
         def boom(*args):
             # why does the new_callable thing need a callable that
             # returns a callable? Feels like I must be doing something
@@ -95,8 +105,9 @@ class EndpointTests(unittest.TestCase):
             return bam
         with patch('txtorcon.endpoints.launch_tor') as m:
             with patch('txtorcon.endpoints.build_tor_connection', new_callable=boom) as btc:
+                client = clientFromString(self.reactor, "tcp:localhost:port=9050")
                 ep = yield TCPHiddenServiceEndpoint.system_tor(self.reactor,
-                                                           clientFromString(self.reactor, "tcp:localhost:port=9050"), 80)
+                                                               client, 80)
                 port = yield ep.listen(NoOpProtocolFactory())
                 toa = port.getHost()
                 self.assertTrue(hasattr(toa, 'onion_uri'))
@@ -105,7 +116,6 @@ class EndpointTests(unittest.TestCase):
                 str(port)
                 port.tor_config
                 m.assert_called()
-        
 
     @defer.inlineCallbacks
     def test_basic(self):
@@ -126,9 +136,6 @@ class EndpointTests(unittest.TestCase):
             # make sure we called listenTCP not connectTCP
             self.assertEqual(e, listen)
 
-        #self.assertIsInstance(port, torconfig.TorOnionListeningPort)
-        #self.assertEqual('127.0.0.1', ep.tcp_endpoint._interface)
-        ## make sure _ListWrapper's __repr__ doesn't explode
         repr(self.config.HiddenServices)
 
     def test_progress_updates(self):
@@ -143,7 +150,6 @@ class EndpointTests(unittest.TestCase):
         # kind-of cheating, test-wise?
         ep._tor_progress_update(*args)
         ding.assert_called_with(*args)
-        
 
     def test_multiple_listen(self):
         ep = TCPHiddenServiceEndpoint(self.reactor, self.config, 123)
@@ -236,7 +242,6 @@ class EndpointLaunchTests(unittest.TestCase):
     def test_onion_parse_unix_socket(self):
         r = Mock()
         ep = serverFromString(r, "onion:80:controlPort=/tmp/foo")
-
 
     @patch('txtorcon.TCPHiddenServiceEndpoint.system_tor')
     @patch('txtorcon.TCPHiddenServiceEndpoint.global_tor')
@@ -331,6 +336,7 @@ class FakeAddress(object):
     def __hash__(self):
         return hash((self.type, self.host, self.port))
 
+
 class FakeListeningPort(object):
     implements(IListeningPort)
 
@@ -346,15 +352,18 @@ class FakeListeningPort(object):
     def getHost(self):
         return FakeAddress('host', self.port)
 
+
 def port_generator():
     for x in xrange(65535, 0, -1):
         yield x
 
 
-from test_torconfig import FakeReactor # FIXME
-from test_torconfig import FakeProcessTransport # FIXME
-from test_torconfig import FakeControlProtocol # FIXME
-class FakeReactorTcp(FakeReactor):#object):
+from test_torconfig import FakeReactor  # FIXME
+from test_torconfig import FakeProcessTransport  # FIXME
+from test_torconfig import FakeControlProtocol  # FIXME
+
+
+class FakeReactorTcp(FakeReactor):
     implements(IReactorTCP)
 
     failures = 0
@@ -366,6 +375,7 @@ class FakeReactorTcp(FakeReactor):#object):
         self.transport = proto_helpers.StringTransport()
         self.transport = FakeProcessTransport()
         self.transport.protocol = self.protocol
+
         def blam():
             self.protocol.outReceived("Bootstrap")
         self.transport.closeStdin = blam
@@ -392,6 +402,7 @@ class FakeReactorTcp(FakeReactor):#object):
 #        print "XX", factory
         r = tcp.Connector(host, port, factory, timeout, bindAddress, reactor=self)
 #        print dir(r)
+
         def blam(*args):
             print "BLAAAAAM", args
         r.connect = blam
