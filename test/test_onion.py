91f0415c000a06b57adf961b05832bcb71dafcd6
meejah <meejah@meejah.ca>
2016-09-16 20:26:48 -0600

    test for ephemeral onion-service upload failure

--- test/test_onion.py
+++ test/test_onion.py
@@ -5,6 +5,7 @@ from zope.interface import implementer
 from mock import Mock
 
 from twisted.trial import unittest
+from twisted.internet import defer
 
 from txtorcon.onion import EphemeralHiddenService
 from txtorcon.onion import IOnionService  # FIXME interfaces.py
@@ -23,3 +24,49 @@ class OnionInterfaceTests(unittest.TestCase):
             EphemeralHiddenService(Mock(), [])
         )
 
+
+class EphemeralServiceTests(unittest.TestCase):
+
+    @defer.inlineCallbacks
+    def test_uploads_fail(self):
+        """
+        When all descriptor uploads fail, we get an error
+        """
+
+        class FakeProtocol(object):
+            listener = None
+
+            def queue_command(s, cmd):
+                return "ServiceID=deadbeefdeadbeef\nPrivateKey=RSA1024:xxxx"
+
+            def add_event_listener(s, evt, listener):
+                self.assertTrue(evt == 'HS_DESC')
+                # should only get one listener added
+                self.assertTrue(s.listener is None)
+                s.listener = listener
+
+        class FakeConfig(object):
+            EphemeralOnionServices=[]
+            tor_protocol = FakeProtocol()
+
+        progress = Mock()
+        config = FakeConfig()
+        hs = EphemeralHiddenService.create(
+            config,
+            ['80 127.0.0.1:80'],
+            progress=progress,
+        )
+
+        for x in range(6):
+            config.tor_protocol.listener('UPLOAD deadbeefdeadbeef x hs_dir_{}'.format(x))
+
+        for x in range(6):
+            config.tor_protocol.listener('FAILED deadbeefdeadbeef x hs_dir_{}'.format(x))
+
+        try:
+            hs = yield hs
+            self.fail("should have gotten exception")
+        except Exception as e:
+            self.assertTrue("Failed to upload 'deadbeefdeadbeef.onion'" in str(e))
+            for x in range(6):
+                self.assertTrue("hs_dir_{}".format(x) in str(e))
