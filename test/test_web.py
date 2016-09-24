2ec888e1359f498144b05c368c59823d261c91ce
meejah <meejah@meejah.ca>
2016-09-23 21:55:55 -0600

    test web-circuit coverage

--- test/test_web.py
+++ test/test_web.py
@@ -5,7 +5,9 @@ from twisted.trial import unittest
 from twisted.internet import defer
 
 from txtorcon.web import agent_for_socks_port
+from txtorcon.web import tor_agent
 from txtorcon.socks import TorSocksEndpoint
+from txtorcon.circuit import TorCircuitEndpoint
 
 
 class WebAgentTests(unittest.TestCase):
@@ -42,6 +44,37 @@ class WebAgentTests(unittest.TestCase):
 
         # do the test
         agent = yield agent_for_socks_port(reactor, config, '127.0.0.50:1234', pool=pool)
+
+        # apart from the getConnection asserts...
+        res = yield agent.request(b'GET', b'https://meejah.ca')
+        self.assertIs(res, gold)
+
+    @defer.inlineCallbacks
+    def test_agent(self):
+        reactor = Mock()
+        socks_ep = Mock()
+        agent = yield tor_agent(reactor, socks_ep)
+
+    @defer.inlineCallbacks
+    def test_agent_with_circuit(self):
+        reactor = Mock()
+        circuit = Mock()
+        socks_ep = Mock()
+        proto = Mock()
+        gold = object()
+        proto.request = Mock(return_value=defer.succeed(gold))
+
+        def getConnection(key, endpoint):
+            self.assertTrue(isinstance(endpoint, TorCircuitEndpoint))
+            target = endpoint._target_endpoint
+            self.assertTrue(target._tls)
+            self.assertEqual(target._host, 'meejah.ca')
+            self.assertEqual(target._port, 443)
+            return defer.succeed(proto)
+        pool = Mock()
+        pool.getConnection = getConnection
+
+        agent = yield tor_agent(reactor, socks_ep, circuit=circuit, pool=pool)
 
         # apart from the getConnection asserts...
         res = yield agent.request(b'GET', b'https://meejah.ca')
