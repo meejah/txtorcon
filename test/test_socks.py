8e0474f7e1ca998e520f68ac6198ec75237ee2c5
meejah <meejah@meejah.ca>
2016-09-27 15:18:53 -0600

    some first-cut SOCKS tests

---
+++ test/test_socks.py
@@ -0,0 +1,73 @@
+
+from mock import Mock
+
+from twisted.trial import unittest
+from twisted.internet import defer
+from twisted.test import proto_helpers
+
+from txtorcon import socks
+
+
+class SocksConnectTests(unittest.TestCase):
+
+    @defer.inlineCallbacks
+    def test_connect_no_tls(self):
+        socks_ep = Mock()
+        transport = proto_helpers.StringTransport()
+
+        def connect(factory):
+            factory.startFactory()
+            proto = factory.buildProtocol("addr")
+            proto.makeConnection(transport)
+            self.assertEqual(b'\x05\x01\x00', transport.value())
+            proto.dataReceived(b'\x05\x00')
+            proto.dataReceived(b'\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00')
+            return proto
+        socks_ep.connect = connect
+        protocol = Mock()
+        factory = Mock()
+        factory.buildProtocol = Mock(return_value=protocol)
+        ep = socks.TorSocksEndpoint(socks_ep, 'meejah.ca', 443)
+        proto = yield ep.connect(factory)
+        self.assertEqual(proto, protocol)
+
+
+class SocksResolveTests(unittest.TestCase):
+
+    @defer.inlineCallbacks
+    def test_resolve(self):
+        socks_ep = Mock()
+        transport = proto_helpers.StringTransport()
+
+        def connect(factory):
+            factory.startFactory()
+            proto = factory.buildProtocol("addr")
+            proto.makeConnection(transport)
+            # XXX sadness: we probably "should" just feed the right
+            # bytes to the protocol to convince it a connection is
+            # made ... *or* we can cheat and just do the callback
+            # directly...
+            proto._done.callback("the dns answer")
+            return proto
+        socks_ep.connect = connect
+        hn = yield socks.resolve(socks_ep, 'meejah.ca')
+        self.assertEqual(hn, "the dns answer")
+
+    @defer.inlineCallbacks
+    def test_resolve_ptr(self):
+        socks_ep = Mock()
+        transport = proto_helpers.StringTransport()
+
+        def connect(factory):
+            factory.startFactory()
+            proto = factory.buildProtocol("addr")
+            proto.makeConnection(transport)
+            # XXX sadness: we probably "should" just feed the right
+            # bytes to the protocol to convince it a connection is
+            # made ... *or* we can cheat and just do the callback
+            # directly...
+            proto._done.callback("the dns answer")
+            return proto
+        socks_ep.connect = connect
+        hn = yield socks.resolve_ptr(socks_ep, 'meejah.ca')
+        self.assertEqual(hn, "the dns answer")
