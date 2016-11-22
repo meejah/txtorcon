13487b1fad2c0b38f91224f8937bf2037add695b
David Stainton <dstainton415@gmail.com>
2016-11-22 01:40:51 +0000

    Use using_guards=False for timeout circuit build test

--- test/test_torstate.py
+++ test/test_torstate.py
@@ -1169,7 +1169,7 @@ s Fast Guard Running Stable Valid
         timeout = 10
         clock = task.Clock()
 
-        d = build_timeout_circuit(self.state, clock, path, timeout, using_guards=True)
+        d = build_timeout_circuit(self.state, clock, path, timeout, using_guards=False)
         clock.advance(10)
 
         def check_for_timeout_error(f):
