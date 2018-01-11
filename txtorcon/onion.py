bee2e4e3dbc1df49ff3e0b82c055b759ce3459ad
meejah <meejah@meejah.ca>
2018-01-11 14:12:12 -0700

    more for travis

--- txtorcon/onion.py
+++ txtorcon/onion.py
@@ -661,7 +661,8 @@ class EphemeralAuthenticatedOnionService(object):
         else:
             blob = self._private_key.encode('ascii')
         keydata = b'-----BEGIN RSA PRIVATE KEY-----\n' + blob + b'\n-----END RSA PRIVATE KEY-----\n'
-        keydata = b'-----BEGIN PRIVATE KEY-----\n' + blob + b'\n-----END PRIVATE KEY-----\n'
+#        keydata = b'-----BEGIN PRIVATE KEY-----\n' + blob + b'\n-----END PRIVATE KEY-----\n'
+        print("keydata:\n{}".format(keydata))
         private_key = serialization.load_pem_private_key(
             keydata,
             password=None,
