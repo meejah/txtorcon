3b091730949e0f552be1f1f16da7ee9030dc86c7
meejah <meejah@meejah.ca>
2019-03-10 18:55:16 -0600

    bump version, check release metadata script

---
+++ scripts/check_release_metadata.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+from os.path import join
+
+
+def main():
+    """
+    This does checks on the release metadata for PyPI and pip-licenses:
+    """
+
+    subprocess.run(
+        [sys.executable, "setup.py", "bdist_wheel"],
+        check=True,
+    )
+    import txtorcon
+    dist_file = join("dist", "txtorcon-{}-py2.py3-none-any.whl".format(txtorcon.__version__))
+    print("dist: {}".format(dist_file))
+    subprocess.run(
+        [sys.executable, "-m", "twine", "check", dist_file],
+        check=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
