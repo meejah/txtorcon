ec8577a099131016d576ca4819288c6046d2e136
meejah <meejah@meejah.ca>
2020-03-30 20:35:28 -0600

    default is py3

--- examples/readme.py
+++ examples/readme.py
@@ -1,45 +1,47 @@
-from __future__ import print_function
-
 from twisted.internet.task import react
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, ensureDeferred
 from twisted.internet.endpoints import UNIXClientEndpoint
+
 import treq
 import txtorcon
 
 
-@react
-@inlineCallbacks
-def main(reactor):
-    tor = yield txtorcon.connect(
+async def main(reactor):
+    tor = await txtorcon.connect(
         reactor,
         UNIXClientEndpoint(reactor, "/var/run/tor/control")
     )
 
     print("Connected to Tor version {}".format(tor.version))
 
-    url = 'https://www.torproject.org:443'
-    print("Downloading {}".format(url))
-    resp = yield treq.get(url, agent=tor.web_agent())
+    url = u'https://www.torproject.org:443'
+    print(u"Downloading {}".format(repr(url)))
+    resp = await treq.get(url, agent=tor.web_agent())
 
-    print("   {} bytes".format(resp.length))
-    data = yield resp.text()
-    print("Got {} bytes:\n{}\n[...]{}".format(
+    print(u"   {} bytes".format(resp.length))
+    data = await resp.text()
+    print(u"Got {} bytes:\n{}\n[...]{}".format(
         len(data),
         data[:120],
         data[-120:],
     ))
 
-    print("Creating a circuit")
-    state = yield tor.create_state()
-    circ = yield state.build_circuit()
-    yield circ.when_built()
-    print("  path: {}".format(" -> ".join([r.ip for r in circ.path])))
+    print(u"Creating a circuit")
+    state = await tor.create_state()
+    circ = await state.build_circuit()
+    await circ.when_built()
+    print(u"  path: {}".format(" -> ".join([r.ip for r in circ.path])))
 
-    print("Downloading meejah's public key via above circuit...")
-    config = yield tor.get_config()
-    resp = yield treq.get(
-        'https://meejah.ca/meejah.asc',
+    print(u"Downloading meejah's public key via above circuit...")
+    config = await tor.get_config()
+    resp = await treq.get(
+        u'https://meejah.ca/meejah.asc',
         agent=circ.web_agent(reactor, config.socks_endpoint(reactor)),
     )
-    data = yield resp.text()
+    data = await resp.text()
     print(data)
+
+
+@react
+def _main(reactor):
+    return ensureDeferred(main(reactor))
