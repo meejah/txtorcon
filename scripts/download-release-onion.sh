1df210b8df3fd226b4d2929d1bf1f82c66f3b7d6
meejah <meejah@meejah.ca>
2021-08-16 13:44:51 -0600

    use v3 onion for release-check

--- scripts/download-release-onion.sh
+++ scripts/download-release-onion.sh
@@ -6,12 +6,12 @@
 
 set -x
 pushd /tmp
-torsocks curl -O http://timaq4ygg2iegci7.onion/txtorcon-${1}.tar.gz || exit $?
-torsocks curl -O http://timaq4ygg2iegci7.onion/txtorcon-${1}.tar.gz.asc || exit $?
+torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}.tar.gz || exit $?
+torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}.tar.gz.asc || exit $?
 gpg --verify txtorcon-${1}.tar.gz.asc || exit 1
 
-torsocks curl -O http://timaq4ygg2iegci7.onion/txtorcon-${1}-py2.py3-none-any.whl || exit $?
-torsocks curl -O http://timaq4ygg2iegci7.onion/txtorcon-${1}-py2.py3-none-any.whl.asc || exit $?
+torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}-py2.py3-none-any.whl || exit $?
+torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}-py2.py3-none-any.whl.asc || exit $?
 gpg --verify txtorcon-${1}-py2.py3-none-any.whl.asc || exit 1
 
 echo "Both binaries check out for version" ${1}
