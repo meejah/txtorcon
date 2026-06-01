49a5a6b854a41ea17bc9ff9da30df01e00b1e17b
meejah <meejah@meejah.ca>
2026-05-31 21:41:04 -0600

    no more py2

--- scripts/download-release-onion.sh
+++ scripts/download-release-onion.sh
@@ -10,9 +10,9 @@ torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid
 torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}.tar.gz.asc || exit $?
 gpg --verify txtorcon-${1}.tar.gz.asc || exit 1
 
-torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}-py2.py3-none-any.whl || exit $?
-torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}-py2.py3-none-any.whl.asc || exit $?
-gpg --verify txtorcon-${1}-py2.py3-none-any.whl.asc || exit 1
+torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}-py3-none-any.whl || exit $?
+torsocks curl -O http://fjblvrw2jrxnhtg67qpbzi45r7ofojaoo3orzykesly2j3c2m3htapid.onion/txtorcon-${1}-py3-none-any.whl.asc || exit $?
+gpg --verify txtorcon-${1}-py3-none-any.whl.asc || exit 1
 
 echo "Both binaries check out for version" ${1}
 popd
